16e044babc37e39636212f53472b8a63a699f835
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2017-02-10 10:43:07 +0100

    Working on gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,1 +1,39 @@
+def creat_game_view(height, width):
+    """
+    Create a new view buffer for the gui.
 
+    Parameter
+    ---------
+    height : height of the game view (int).
+    width : width of the game view (int).
+
+    Return
+    ------
+    game_view : the new game view (dic).
+    """
+
+    raise NotImplemented
+
+def draw_rectangle(x1,y1,x2,y2):
+    """
+    Draw a rectangle in the string buffer.
+
+    Parameters
+    ----------
+    x1, y1, x2, y2 : coordinate of the rectangle (int).
+
+    return
+    ------
+    game_view : game view whith the rectangle (dic).
+    """
+
+    raise NotImplemented
+
+def print_game_view(game_view):
+    """
+    Print the game view in the terminal.
+
+    Parameter
+    ---------
+    game_view : string buffer to draw on screen.
+    """
