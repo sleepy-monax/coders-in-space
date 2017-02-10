8496142e9d38dc76fc51de795d9c5a0acde6c177
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2017-02-10 12:49:07 +0100

    Working on GUI.py
    Voila je continue a travailler sur l'interface :smile:

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,10 @@
-def creat_game_view(height, width):
+"""
+=====
+=GUI= This section of code is use for all the user inteface stuff.
+=====
+"""
+
+def creat_game_view(width, height):
     """
     Create a new view buffer for the gui.
 
@@ -10,24 +16,89 @@ def creat_game_view(height, width):
     Return
     ------
     game_view : the new game view (dic).
+
+    Version
+    -------
+    specification v1. Nicolas Van Bossuyt (10/2/2017)
+    implementation v1. Nicolas Van Bossuyt (10/2/2017)
     """
 
-    raise NotImplemented
+    game_view = {'size': (width, height), 'grid': {}}
 
-def draw_rectangle(x1,y1,x2,y2):
+    for x in range(width):
+        for y in range(height):
+            game_view['grid'][(x,y)] = ' '
+
+    return game_view
+
+
+def put(x, y, char, game_view):
+    """
+    Put the specified char in the game_view.
+
+    Parameter
+    ---------
+    x, y : coordinate of were to put the char (int).
+    char : char to put (str).
+
+    Version
+    -------
+    specification v1. Nicolas Van Bossuyt (10/2/2017)
+    implementation v1. Nicolas Van Bossuyt (10/2/2017)
+    """
+
+    if x < game_view['size'][0] and x>=0 and\
+    y < game_view['size'][1] and y >= 0:
+        game_view['grid'][(x,y)] = char
+
+    return game_view
+
+def put_rectangle(x,y, width, height, char, game_view):
     """
     Draw a rectangle in the string buffer.
 
     Parameters
     ----------
-    x1, y1, x2, y2 : coordinate of the rectangle (int).
+    x, y : coordinate of the rectangle (int).
+    width, height : size of the rectangle (int).
 
     return
     ------
     game_view : game view whith the rectangle (dic).
+
+    Version
+    -------
+    specification v1. Nicolas Van Bossuyt (10/2/2017)
+    implementation v1. Nicolas Van Bossuyt (10/2/2017)
     """
 
     raise NotImplemented
+
+def put_string(x, y, string, direction_x, direction_y, game_view):
+    """
+    Put a specified string in the game_view.
+
+    Parameter
+    ---------
+    x, y : coordinate of the string (int).
+    direction_x, direction_y : direction to draw the string (int).
+
+    Return
+    ------
+    game_view : game view with the new string (dic).
+
+    Version
+    -------
+    specification v1. Nicolas Van Bossuyt (10/2/2017)
+    implementation v1. Nicolas Van Bossuyt (10/2/2017)
+    """
+
+    for char in string:
+        game_view = put(x, y, char, game_view)
+        x += direction_x
+        y += direction_y
+
+    return game_view
 
 def print_game_view(game_view):
     """
@@ -36,4 +107,16 @@ def print_game_view(game_view):
     Parameter
     ---------
     game_view : string buffer to draw on screen.
+
+    Version
+    -------
+    specification v1. Nicolas Van Bossuyt (10/2/2017)
+    implementation v1. Nicolas Van Bossuyt (10/2/2017)
     """
+
+    for i in game_view:
+        line = ''
+        for j in i:
+            line += j
+
+        print line
