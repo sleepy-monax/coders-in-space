43a0fdbf559bf2336d5acf8ac354b94923138dda
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2017-02-10 16:22:28 +0100

    Working on GUI.py

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,9 @@
+from termcolor import *
 """
 =====
 =GUI= This section of code is use for all the user inteface stuff.
 =====
 """
-
 def creat_game_view(width, height):
     """
     Create a new view buffer for the gui.
@@ -27,20 +27,20 @@ def creat_game_view(width, height):
 
     for x in range(width):
         for y in range(height):
-            game_view['grid'][(x,y)] = ' '
+            game_view['grid'][(x,y)] = {'color':'white', 'back_color':'on_black', 'char':' '}
 
     return game_view
 
-
-def put(x, y, char, game_view):
+def put(game_view, x, y, char, color = 'white', back_color = 'on_black'):
     """
     Put the specified char in the game_view.
 
     Parameter
     ---------
+    game_view : game view to put the char in (dic).
     x, y : coordinate of were to put the char (int).
     char : char to put (str).
-    game_view : game view to put the char in (dic).
+    color, back_color : color for the char (string).
 
     Version
     -------
@@ -51,12 +51,15 @@ def put(x, y, char, game_view):
     # Check if the coordinate is in the bound of the game view.
     if x < game_view['size'][0] and x>=0 and\
     y < game_view['size'][1] and y >= 0:
+
         # Put the char a the coordinate.
-        game_view['grid'][(x,y)] = char
+        game_view['grid'][(x,y)]['char'] = char
+        game_view['grid'][(x,y)]['color'] = color
+        game_view['grid'][(x,y)]['back_color'] = back_color
 
     return game_view
 
-def put_rectangle(x,y, width, height, char, game_view):
+def put_rectangle(game_view, x, y, width, height, char, color = 'white', back_color = 'on_black'):
     """
     Draw a rectangle in the string buffer.
 
@@ -77,11 +80,11 @@ def put_rectangle(x,y, width, height, char, game_view):
 
     for w in range(width):
         for h in range(height):
-            game_view = put(x + w, y + h, char, game_view)
+            game_view = put(game_view, x + w, y + h, char, color, back_color)
 
     return game_view
 
-def put_string(x, y, string, direction_x, direction_y, game_view):
+def put_string(game_view, x, y, string, direction_x = 1, direction_y = 0, color = 'white', back_color = 'on_black'):
     """
     Put a specified string in the game_view.
 
@@ -106,7 +109,7 @@ def put_string(x, y, string, direction_x, direction_y, game_view):
     """
 
     for char in string:
-        game_view = put(x, y, char, game_view)
+        game_view = put(game_view, x, y, char, color, back_color)
         x += direction_x
         y += direction_y
 
@@ -126,11 +129,24 @@ def print_game_view(game_view):
     implementation v1. Nicolas Van Bossuyt (10/2/2017)
     """
 
-    for i in game_view:
-        line = ''
-        for j in i:
-            line += j
+    game_view_width = game_view['size'][0]
+    game_view_height = game_view['size'][1]
 
-        print line
+    for x in range(game_view_width):
+        for y in range(game_view_height):
+            grid_item = game_view['grid'][(x,y)]
+            char = grid_item['char']
+            color = grid_item['color']
+            back_color = grid_item['back_color']
 
-def print_game_
+            cprint(char, color, back_color)
+
+        cprint('\n')
+
+
+
+def try_gui_lib():
+    v = creat_game_view(10,10)
+    put_rectangle(v,3,3,3,3,'B', 'red')
+    put_string(v, 4,4, 'Ca marche !')
+    print_game_view(v)
