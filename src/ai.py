01c6f57f94c25431bb69c2d48fc96a7ff8995ae3
Nicolas <nicovb30@hotmail.com>
2017-02-10 19:03:09 +0100

    Add specification for get_ai_input()

--- src/ai.py
+++ src/ai.py
@@ -1,1 +1,13 @@
+def get_ai_input(game_stats):
+    """
+    Get the game input from the ai.
 
+    Parameter
+    ---------
+    game_stats : game stat of the current game (dic).
+
+    Return
+    ------
+    ai_input : game input from the ai (str).
+    """
+    raise NotImplemented
