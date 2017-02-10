fca9837ec6a13174e41103e6b2b63f37c29ed80d
Nicolas <nicovb30@hotmail.com>
2017-02-10 18:41:17 +0100

    Update src/gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from termcolor import *
 """
 =====
@@ -8,7 +9,7 @@ def creat_game_view(width, height):
     """
     Create a new view buffer for the gui.
 
-    Parameter
+    Parameterr
     ---------
     height : height of the game view (int).
     width : width of the game view (int).
@@ -66,6 +67,7 @@ def put_rectangle(game_view, x, y, width, height, char, color = None, back_color
     ----------
     x, y : coordinate of the rectangle (int).
     width, height : size of the rectangle (int).
+    color, back_color : color for the char (string).
 
     return
     ------
@@ -80,6 +82,39 @@ def put_rectangle(game_view, x, y, width, height, char, color = None, back_color
     for w in range(width):
         for h in range(height):
             game_view = put(game_view, x + w, y + h, char, color, back_color)
+
+    return game_view
+
+def put_box(game_view, x, y, width, height, mode = 'double', color = None, back_color = None):
+    """
+    Put a box in the game view.
+
+    Parameters
+    ----------
+    x, y : coordinate of the rectangle (int).
+    width, height : size of the rectangle (int).
+    mode : double ou single line <'double'|'single'> (str).
+    color, back_color : color for the char (string).
+
+    return
+    ------
+    game_view : game view whith the rectangle (dic).
+
+    Version
+    -------
+    specification v1. Nicolas Van Bossuyt (10/2/2017)
+    implementation v1. Nicolas Van Bossuyt (10/2/2017)
+    """
+    if mode == 'double':
+        raise NotImplemented
+
+    elif mode == 'single':
+        put_rectangle(game_view, x, y, width, height, u'─', color, back_color)
+        put_rectangle(game_view, x, y + 1, width, height - 2,u'│', color, back_color)
+        put(game_view, x, y, u'┌', color, back_color)
+        put(game_view, x, y + height - 1, u'└', color, back_color)
+        put(game_view, x + width - 1, y, u'┐', color, back_color)
+        put(game_view, x + width - 1, y + height - 1, u'┘', color, back_color)
 
     return game_view
 
@@ -131,23 +166,24 @@ def print_game_view(game_view):
     game_view_width = game_view['size'][0]
     game_view_height = game_view['size'][1]
 
-    for x in range(game_view_width):
-        for y in range(game_view_height):
+
+    for y in range(game_view_height):
+        line = ''
+        for x in range(game_view_width):
             grid_item = game_view['grid'][(x,y)]
             char = grid_item['char']
             color = grid_item['color']
             back_color = grid_item['back_color']
+            line = line + colored(char, color, back_color)
 
-            cprint(char, color, back_color)
-
-        cprint('\n')
+        print line
 
 
 
 def try_gui_lib():
-    v = creat_game_view(10,10)
-    put_rectangle(v,3,3,3,3,'B', 'red')
-    put_string(v, 4,4, 'Ca marche !', 1, 1,'blue')
+    v = creat_game_view(20,20)
+    put_box(v, 0,0, 19, 3, 'single', 'yellow', 'on_blue')
+    put_string(v, 1,1,' Code In Space ! ',1,0, 'yellow', 'on_blue')
     print_game_view(v)
 
 try_gui_lib()
