721f96e32a459bdb813811394d849457adfb9610
Nicolas <nicovb30@hotmail.com>
2017-02-10 19:19:20 +0100

    ADD comments

--- src/codeinspace.py
+++ src/codeinspace.py
@@ -24,6 +24,8 @@ def new_game(path, list_players):
     implementation : Bayron Mahy (v1. 10/02/2017)
                      Bayron Mahy (v2. 10/02/2017)
     """
+    
+    # Create game_stats dictionary.
 	game_stats = {'board':{}, 'players':{}, 'rounds': 0, 'max_nb_rounds': 10*len(list_players), 'model_ship':{}, 'ship' {}}
 	game_file = parse_game_file(path)
 
@@ -40,6 +42,8 @@ def new_game(path, list_players):
 			type= 'human'
 
 		game_stats['players'][player]={'money':100, 'nb_ship': 0, 'type':type}
+
+    # Create ship specs sheet.
 	game_stats['model_ship']['fighter']={'max_heal':3, 'max_speed':5, 'damages':1, 'range':5, 'price':10}
 	game_stats['model_ship']['destroyer']={'max_heal':8, 'max_speed':2, 'damages':2, 'range':7, 'price':20}
 	game_stats['model_ship']['battlecruiser']={'max_heal':20, 'max_speed':1, 'damages':4, 'range':10, 'price':30}
