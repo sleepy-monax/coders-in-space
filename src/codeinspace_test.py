8812faf798de3d6e7d6116dacc11ccb7c7327f0c
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2017-02-16 10:58:36 +0100

    Add test for show_board() and parse_game_file()

--- src/codeinspace_test.py
+++ src/codeinspace_test.py
@@ -2,13 +2,25 @@ import pytest
 import codeinspace
 import os
 
+def test_parse_game_file():
+    try:
+        codeinspace.parse_game_file('board/test0.cis')
+        assert True
+    except Exception as e:
+        raise
+        assert False
+        
 def test_new_game():
-    print os.getcwd()
-
-
     try:
         codeinspace.new_game('board/test0.cis', ('Vador','ai'))
         assert True
     except Exception as e:
-        print e
+        assert False
+
+def test_show_board():
+    try:
+        codeinspace.show_board(codeinspace.new_game('board/test0.cis', ('Vador', 'ai')))
+        assert True
+    except Exception as e:
+        raise
         assert False
