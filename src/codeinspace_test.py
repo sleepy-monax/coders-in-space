77d79f2748a5e63ebafe9deb3e4c5e1e2d0cb1a4
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2017-02-16 10:34:10 +0100

    fix test

--- src/codeinspace_test.py
+++ src/codeinspace_test.py
@@ -1,9 +1,14 @@
 import pytest
 import codeinspace
+import os
 
 def test_new_game():
+    print os.getcwd()
+
+
     try:
-        codeinspace.new_game('board\test0.py', ('Vador','ai'))
+        codeinspace.new_game('board\\test0.cis', ('Vador','ai'))
         assert True
     except Exception as e:
+        print e
         assert False
