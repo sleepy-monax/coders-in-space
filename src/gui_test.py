727dd88430fc7b90bc8e181105203d189ac6ae51
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2017-02-17 09:45:12 +0100

    Update gui_test

--- src/gui_test.py
+++ src/gui_test.py
@@ -4,12 +4,12 @@ import gui
 def test_creat_canvas():
 
     for x in [1, 2, 4, 8, 16, 20, 40, 60]:
-            gui.creat_canvas(x, x, True)
+            gui.create_canvas(x, x, True)
 
     assert True
 
 def test_put():
-    c = gui.creat_canvas(50, 50, True)
+    c = gui.create_canvas(50, 50, True)
 
     gui.put_box(c, 0, 0, 50, 50)
     gui.put_rectangle(c, 0, 0, 25, 25, 'A')
