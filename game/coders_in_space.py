c111760e6c0fd81e0bead3d05d39800a1e35aa94
Nicolas VAN BOSSUYT <nvanboss@simon06.pool.fundp.ac.be>
2017-03-02 11:47:54 +0100

    Fix GUI

--- game/coders_in_space.py
+++ game/coders_in_space.py
@@ -65,10 +65,13 @@ def play_game(level_name, players_list, no_splash = False, screen_size = (190, 5
 
 	game_stats = new_game(level_name, players_list)
 	game_stats['screen_size'] = screen_size
+	
 	if not no_splash: 
 		splash_game(game_stats)
 		raw_input() # wait the player pressing enter.
-		
+
+	show_game_board(game_stats)
+			
 	# Players create their ships.
 	for player in game_stats['players']:
 		game_stats = command_buy_ships(get_game_input(player, True, game_stats), player, game_stats)
@@ -402,7 +405,7 @@ def get_human_input(player_name, buy_ship, game_stats):
 
 	while True:
 		# Getting human player input.
-		player_input = raw_input('\033[%d;%dH %s :' % (50, 3, player_name))
+		player_input = raw_input('\033[%d;%dH %s :' % (game_stats['screen_size'][0], 3, player_name))
 
 		# Run human player command.
 		if '/' in player_input:
