2397448938925762ffb340a389e62cda7164fa0c
Nicolas Van Bossuyt <nicovb30@hotmail.com>
2017-03-03 13:41:07 +0100

    Tiny fix

--- game/run_game.py
+++ game/run_game.py
@@ -1,4 +1,4 @@
 from coders_in_space import *
 raw_input()
-play_game('board/board_1303.cis', ('John_bot', 'Smith_bot', 'rand_bot', 'easyPYsi_bot'), False, (190, 56))
+play_game('board/board_1303.cis', ('John_bot', 'Smith_bot', 'rand_bot', 'easyPYsi_bot'), False, (190, 50))
 raw_input()
