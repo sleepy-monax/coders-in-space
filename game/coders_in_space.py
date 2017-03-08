2c207a5695c397489a4b2f1ba7fb663400002172
nicolas <nicolas.van.bossuyt@gmail.com>
2017-03-08 16:45:12 +0100

    Fix game

--- game/coders_in_space.py
+++ game/coders_in_space.py
@@ -36,7 +36,7 @@
 # Import some cool component for the game.
 from time import sleep #because everyone needs to rest.
 from math import *
-from random import randint
+from random import randint, seed
 from remote_play import notify_remote_orders, get_remote_orders, connect_to_player, disconnect_from_player
 
 # Game
