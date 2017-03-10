c96c3324d4651c0f05e7aa7e3e5afb114e676eff
nicolas <nicolas.van.bossuyt@gmail.com>
2017-03-10 18:09:42 +0100

    Code cleanup

--- game/coders_in_space_test.py
+++
@@ -1,9 +0,0 @@
-from coders_in_space import *
-
-def game_test():
-	try:
-		play_game('board/test_board.cis', ('bob_bot', 'john_bot'), no_splash = True)
-		assert True
-	except Exception as e:
-		raise
-		assert False
